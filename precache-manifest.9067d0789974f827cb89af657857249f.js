d959b9adb12b8a2289bdd13fed5bdb782f766c0b
Karin <KinSweden@users.noreply.github.com>
2020-08-28 10:34:30 +0200

    Updates

---
+++ precache-manifest.9067d0789974f827cb89af657857249f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35bffedce4dfe126b9b547dba728f233",
+    "url": "/Quizztutorial/index.html"
+  },
+  {
+    "revision": "d5a9e0373a02cf0b61f5",
+    "url": "/Quizztutorial/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "64167232648fd53af58f",
+    "url": "/Quizztutorial/static/js/2.e1bf32df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Quizztutorial/static/js/2.e1bf32df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5a9e0373a02cf0b61f5",
+    "url": "/Quizztutorial/static/js/main.af1d143d.chunk.js"
+  },
+  {
+    "revision": "e5b8b8abf13932585400",
+    "url": "/Quizztutorial/static/js/runtime-main.95ccaab6.js"
+  }
+]);
